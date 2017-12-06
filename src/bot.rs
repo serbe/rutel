9ecd2b1774d6dff5af3fac8d761488dd43a6fe4c
Valeriy Kostikov <mkoctuk@gmail.com>
2017-12-06 17:04:42 +0300

    add create_request_with_values

--- src/bot.rs
+++ src/bot.rs
@@ -1,10 +1,10 @@
 use tokio_core::reactor::Core;
-use hyper::{Client, Body};
+use hyper::{Client, Body, Method, Request};
 use hyper_tls::{HttpsConnector};
 use hyper::client::HttpConnector;
 use serde_json::{from_slice, from_value, Value, Error};
-//use hyper::header::{ContentLength, ContentType};
-use types::{Response};
+use hyper::header::{ContentLength, ContentType};
+use types::{Response, User};
 use std::io;
 use futures::future::Future;
 use futures::Stream;
@@ -12,7 +12,7 @@ use futures::Stream;
 #[derive(Debug)]
 pub struct Bot {
     token: String,
-    core: Core,
+    event_loop: Core,
     client: Client<HttpsConnector<HttpConnector>, Body>,
 //    user: Option<User>
 }
@@ -25,14 +25,9 @@ impl Bot {
             .build(&core.handle());
         Bot {
             token: token.to_string(),
-            core: core,
-            client: client,
+            event_loop: core,
+            client,
         }
-    }
-
-    pub fn get_me(self) -> () {
-        let req = self.create_request("getMe");
-        println!("{:?}", req);
     }
 
     fn create_request(mut self, method: &str) -> Result<Response, Error> {
@@ -48,7 +43,43 @@ impl Bot {
                 Ok(v)
             })
         });
-        from_value(self.core.run(work).unwrap())
+        from_value(self.event_loop.run(work).unwrap())
+    }
+
+    fn create_request_with_values(mut self, method: &str, values: &str) -> Result<Response, Error> {
+        let uri = format!("https://api.telegram.org/bot{}/{}", self.token, method).parse().unwrap();
+        let mut req = Request::new(Method::Post, uri);
+        req.headers_mut().set(ContentType::json());
+        req.headers_mut().set(ContentLength(values.len() as u64));
+        req.set_body(&values);
+        let work = self.client.request(req).and_then(|res| {
+            res.body().concat2().and_then(move |body| {
+                let v: Value = from_slice(&body).map_err(|e| {
+                    io::Error::new(
+                        io::ErrorKind::Other,
+                        e
+                    )
+                })?;
+                Ok(v)
+            })
+        });
+        from_value(self.event_loop.run(work).unwrap())
+    }
+
+    pub fn get_me(self) -> Result<User, Error> {
+        let resp = self.create_request("getMe");
+        match resp {
+            Ok(v) => from_value(v.result),
+            Err(e) => Err(e),
+        }
+    }
+
+    pub fn get_updates(self) -> Result<User, Error> {
+        let resp = self.create_request_with_values("getUpdates", "");
+        match resp {
+            Ok(v) => from_value(v.result),
+            Err(e) => Err(e),
+        }
     }
 //    fullURL := urlAPI + bot.Token + "/" + method
 //    resp, err := bot.Client.PostForm(fullURL, values)
