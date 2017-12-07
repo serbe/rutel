c1f8cab1df48937c31a576a1a9d9e2771ba75b4a
Valeriy Kostikov <mkoctuk@gmail.com>
2017-12-07 23:04:10 +0300

    fix getUpdatesParams

--- src/methods.rs
+++ src/methods.rs
@@ -1,17 +1,19 @@
-#[allow(non_camel_case_types)]
-
 use types::*;
 use serde_json::to_string;
 
-// Use this method to receive incoming updates using long polling (wiki). An Array of Update objects is returned.
+/// Use this method to receive incoming updates using long polling (wiki). An Array of Update objects is returned.
 #[derive(Serialize, Debug)]
 pub struct getUpdatesParams {
+    /// Identifier of the first update to be returned. Must be greater by one than the highest among the identifiers of previously received updates. By default, updates starting with the earliest unconfirmed update are returned. An update is considered confirmed as soon as getUpdates is called with an offset higher than its update_id. The negative offset can be specified to retrieve updates starting from -offset update from the end of the updates queue. All previous updates will forgotten.
     #[serde(skip_serializing_if = "Option::is_none")]
     offset: Option<Integer>,
+    /// Limits the number of updates to be retrieved. Values between 1—100 are accepted. Defaults to 100.
     #[serde(skip_serializing_if = "Option::is_none")]
     limit: Option<Integer>,
+    /// Timeout in seconds for long polling. Defaults to 0, i.e. usual short polling. Should be positive, short polling should be used for testing purposes only.
     #[serde(skip_serializing_if = "Option::is_none")]
     timeout: Option<Integer>,
+    /// List the types of updates you want your bot to receive. For example, specify [“message”, “edited_channel_post”, “callback_query”] to only receive updates of these types. See Update for a complete list of available update types. Specify an empty list to receive all updates regardless of type (default). If not specified, the previous setting will be used.
     #[serde(skip_serializing_if = "Option::is_none")]
     allowed_updates: Option<Vec<String>>,
 }
@@ -26,20 +28,24 @@ impl getUpdatesParams {
         }
     }
 
-    pub fn offset(&mut self, v: u64) {
+    pub fn offset(&mut self, v: Integer) -> &mut Self {
         self.offset = Some(v);
+        self
     }
 
-    pub fn limit(&mut self, v: u64) {
+    pub fn limit(&mut self, v: Integer) -> &mut Self {
         self.limit = Some(v);
+        self
     }
 
-    pub fn timeout(&mut self, v: u64) {
+    pub fn timeout(&mut self, v: Integer) -> &mut Self {
         self.timeout = Some(v);
+        self
     }
 
-    pub fn allowed_updates(&mut self, v: Vec<String>) {
+    pub fn allowed_updates(&mut self, v: Vec<String>) -> &mut Self {
         self.allowed_updates = Some(v);
+        self
     }
 
     pub fn json(&self) -> String {
