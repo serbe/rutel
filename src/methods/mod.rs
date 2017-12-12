08cecb1831b9afb846bd9419927f41bd5c5d77c5
Valeriy Kostikov <mkoctuk@gmail.com>
2017-12-12 10:02:44 +0300

    add sendVoice

--- src/methods/mod.rs
+++ src/methods/mod.rs
@@ -7,6 +7,7 @@ pub mod send_photo;
 pub mod send_audio;
 pub mod send_document;
 pub mod send_video;
+pub mod send_voice;
 
 pub use self::get_updates::*;
 pub use self::send_message::*;
@@ -15,3 +16,4 @@ pub use self::send_photo::*;
 pub use self::send_audio::*;
 pub use self::send_document::*;
 pub use self::send_video::*;
+pub use self::send_voice::*;
