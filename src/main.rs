c9fccae91f9e3d324cfd85bd779f269ea4c8b757
Valeriy Kostikov <mkoctuk@gmail.com>
2018-01-12 23:38:23 +0300

    send message

--- src/main.rs
+++ src/main.rs
@@ -41,9 +41,9 @@ fn main() {
     let u = b.get_updates(params.limit(Some(2)));
     //    println!("{:?}", params.json());
     println!("{:?}", u);
-    //    let params = SendMessageParams::new(types::ChatID::from(94_983_903), String::from("Привет"));
-    //    let u = b.send_message(params.json());
-    //    println!("{:?}", u);
+    let mut params = SendMessageParams::new(types::ChatID::from(94_983_903), String::from("Привет"));
+    let u = b.send_message(&mut params);
+    println!("{:?}", u);
     //    println!("{:?}", params.json());
     //    let mut params = ForwardMessageParams::new(types::ChatID::from(94_983_903), types::ChatID::from(94_983_903), 68);
     //    let u = b.forward_message(params.disable_notification(true).json());
