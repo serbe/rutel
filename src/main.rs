663c5d454be9c25b5d47ddcdd124aae255b39a84
Valeriy Kostikov <mkoctuk@gmail.com>
2018-01-16 21:13:31 +0300

    less debug

--- src/main.rs
+++ src/main.rs
@@ -29,24 +29,16 @@ pub use self::params::*;
 use bot::Bot;
 
 fn main() {
-    const TOKEN: &str = "123786433:AAFCmpVP5h491DK2Rjz5ofW5cTO-N2iFmhU";
+    const TOKEN: &str = "123786483:AAFCmpVP5h491DK2Rjz50fW5cTO-N2iFmhU";
 
     let mut b: Bot = Bot::new(TOKEN);
     println!("{:?}", b);
     let u = b.get_me();
     println!("{:?}", u);
     let mut params = GetUpdatesParams::new();
-    // ;
-    // println!("{:?}", params);
     let u = b.get_updates(params.limit(Some(2)));
-    //    println!("{:?}", params.json());
     println!("{:?}", u);
     let mut params = SendMessageParams::new(types::ChatID::from(94_983_903), String::from("Привет"));
     let u = b.send_message(&mut params);
     println!("{:?}", u);
-    //    println!("{:?}", params.json());
-    //    let mut params = ForwardMessageParams::new(types::ChatID::from(94_983_903), types::ChatID::from(94_983_903), 68);
-    //    let u = b.forward_message(params.disable_notification(true).json());
-    //    println!("{:?}", u);
-    //    println!("{:?}", params.json());
 }
