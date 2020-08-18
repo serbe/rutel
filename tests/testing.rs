b4c0c913ce004ca4b7471abc121b1599e212d39f
Valeriy Kostikov <mkoctuk@gmail.com>
2020-08-18 20:46:11 +0300

    no working test

--- tests/testing.rs
+++ tests/testing.rs
@@ -14,3 +14,12 @@ async fn test_get_me() {
     let u = b.get_me().await.unwrap();
     assert!(u.is_bot, true);
 }
+
+#[tokio::test]
+async fn test_send_message() {
+    let token: String = dotenv::var("TOKEN").unwrap();
+    let target: String = dotenv::var("TARGET").unwrap();
+    let mut b = bot::Bot::new(&token);
+    let u = b.send_message(&mut bot::SendMessage::new(target.into(), String::from("Hello"))).await;
+    assert!(dbg!(u).is_ok(), true);
+}
